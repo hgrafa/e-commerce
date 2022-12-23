7762f6706ee305cc0f337595b5cd7c21452f6315
hgrafa <hugorafell@gmail.com>
2022-12-23 19:18:32 -0300

    Inicial commit

---
+++ ecomerce/src/test/java/com/washington/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washington.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
